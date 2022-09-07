42efc46b4096f198bcf340b3218a347ee97b82a4
modous <mohamed1010@live.nl>
2022-09-07 12:44:23 +0200

    js button added(issues)
    have to click button 2 times to dissapear :(

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,18 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+
 
 function expand () {
   document.body.classList.toggle('expand')
 }
+
+function showDiv() {
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', () => {
+    //hide button
+    btn.style.display = 'none';
+    });
+  document.getElementById('welcomeDiv').style.display = "block";
+
+  
+
+
+};
